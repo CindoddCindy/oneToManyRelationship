60d424813f542306fbd0c328ef2e4026bb0f7d07
CindoddCindy <cindodcindy@gmail.com>
2020-12-28 14:58:13 +0700

    belum di run

--- onetomanyrelation/src/main/java/onetomanytest/onetomanyrelation/OnetomanyrelationApplication.java
+++ onetomanyrelation/src/main/java/onetomanytest/onetomanyrelation/OnetomanyrelationApplication.java
@@ -2,8 +2,10 @@ package onetomanytest.onetomanyrelation;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class OnetomanyrelationApplication {
 
 	public static void main(String[] args) {
